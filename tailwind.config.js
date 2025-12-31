f0ffce392697db41545594235bb73f4f6dff1177
daengridwan2 <daengridwan02@gmail.com>
2026-01-01 02:58:17 +0700

    fix the deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: [
+    "./*.html",
+    "./script.js",
+    "./src/**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
